e747c1a263145dc7819e6227c3b4ee65d08bc136
liavictor <46006467+liavictor@users.noreply.github.com>
2020-10-20 10:31:54 -0400

    Course project 1: plot #1 R codes

---
+++ plot1.R
@@ -0,0 +1,17 @@
+### Exploratory Data Analysis Course Project 1: Codes to create plot #1 Histogram for "Global Activ Power"
+### make sure to download source data and unzip file in the working directory.
+
+## step 1 load the data from unzipped text file
+SourceData <- "./household_power_consumption.txt"
+data <- read.table(SourceData, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+## step 2 subset the data: "We will only be using data from the dates 2007-02-01 and 2007-02-02"
+subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## step 3 plot the #1 graph on screen for visualization in R
+hist(as.numeric(subData$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+
+## step 4 plot the #1 graph png file in the working directory.
+png("plot1.png", width=480, height=480)
+hist(as.numeric(subData$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
